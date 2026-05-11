8b72eb9be536177310ccda9a19c7cc6ef846bc56
groofazy <gabe.gamboa05@gmail.com>
2026-05-10 23:47:32 -0700

    update: path parameters

--- backend/main.py
+++ backend/main.py
@@ -7,3 +7,39 @@ async def root():
     return {
         "message" : "Hello Keyanna"
     }
+
+game = {
+    "game_id": 1,
+    "live_status": 2,
+    "home_team": "Celtics",
+    "away_team": "76ers",
+    "arena": "TD Garden",
+    "game_type": "Playoff"
+}
+
+@app.get("/games/{game_id}")
+async def read_game(game_id: int):
+    return game
+
+games = [
+    {
+        "game_id": 1,
+        "live_status": 2,
+        "home_team": "Celtics",
+        "away_team": "76ers",
+        "arena": "TD Garden",
+        "game_type": "Playoff"
+    },
+    {
+        "game_id": 2,
+        "live_status": 1,
+        "home_team": "Lakers",
+        "away_team": "Warriors",
+        "arena": "Crypto.com Arena",
+        "game_type": "Playoff"
+    }
+]
+
+@app.get("/games")
+async def read_games():
+    return games
